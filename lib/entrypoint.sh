40e9be402a0b6ff1fa9348d9bd4d036f8183cf30
Antoni Baum <antoni.baum@gmail.com>
2019-11-18 23:47:01 +0100

    Fix

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -18,7 +18,7 @@ mv $GITHUB_WORKSPACE/hoi4.cwb.7z .
 p7zip -d hoi4.cwb.7z
 
 cwtools --game hoi4 --directory "$GITHUB_WORKSPACE" --cachefile "/hoi4.cwb" --rulespath "/src/cwtools-hoi4-config/Config" validate --reporttype json --scope mods --outputfile output.json all
-echo "$GITHUB_WORKSPACE/artifact"
-mkdir -p -v "$GITHUB_WORKSPACE/artifact"
-cp -v output.json "$GITHUB_WORKSPACE/artifact"
+
+mkdir -p -v $GITHUB_WORKSPACE/artifact
+cp -v output.json $GITHUB_WORKSPACE/artifact
 ruby /action/lib/cwtools.rb
