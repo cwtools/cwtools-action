e1c3d36267fd791335e28f87362aa9b98dc284e0
Antoni Baum <antoni.baum@gmail.com>
2019-11-29 22:32:24 +0100

    git clone instead of wget

--- lib/gitlab_setup.sh
+++ lib/gitlab_setup.sh
@@ -6,11 +6,10 @@ if [ -z "$INPUT_REVIEWDOGREF" ] || [ "$INPUT_REVIEWDOGREF" = "" ]; then
     INPUT_REVIEWDOGREF="master"
 fi
 
-mkdir /action
-mkdir /action/lib
-wget https://raw.githubusercontent.com/cwtools/cwtools-action/$INPUT_CWTOOLSACTIONREF/lib/entrypoint.sh -O /action/lib/entrypoint.sh
-wget https://raw.githubusercontent.com/cwtools/cwtools-action/$INPUT_CWTOOLSACTIONREF/lib/cwtools.rb -O /action/lib/cwtools.rb
 apt-get update && apt-get -y install ruby bash git wget p7zip
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/$INPUT_REVIEWDOGREF/install.sh| sh -s -- -b /usr/local/bin/
+
+cd /
+git clone --depth=1  --single-branch --branch $INPUT_CWTOOLSACTIONREF https://github.com/cwtools/cwtools-cache-files.git action
 chmod +x /action/lib/entrypoint.sh
 /action/lib/entrypoint.sh
