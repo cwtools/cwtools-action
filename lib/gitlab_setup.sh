0135f7852b4a0dc3eda9bc44216fde41418593ef
Antoni Baum <antoni.baum@gmail.com>
2019-11-29 22:42:10 +0100

    Fix

--- lib/gitlab_setup.sh
+++ lib/gitlab_setup.sh
@@ -10,6 +10,6 @@ apt-get update && apt-get -y install ruby bash git wget p7zip
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/$INPUT_REVIEWDOGREF/install.sh| sh -s -- -b /usr/local/bin/
 
 cd /
-git clone --depth=1  --single-branch --branch $INPUT_CWTOOLSACTIONREF https://github.com/cwtools/cwtools-cache-files.git action
+git clone --depth=1  --single-branch --branch $INPUT_CWTOOLSACTIONREF https://github.com/cwtools/cwtools-action.git action
 chmod +x /action/lib/entrypoint.sh
 /action/lib/entrypoint.sh
