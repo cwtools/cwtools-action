ffca7880ba8cd5ba22aba255beb19d2165ff080d
Antoni Baum <antoni.baum@gmail.com>
2019-11-30 02:02:59 +0100

    Fix

--- lib/cwtools.rb
+++ lib/cwtools.rb
@@ -100,7 +100,7 @@ if @CW_CI_ENV == "github"
     "User-Agent": 'cwtools-action'
   }
 elsif @CW_CI_ENV == "gitlab"
-  @event = JSON.parse(File.read("https://gitlab.com/api/v4/projects/#{ENV["CI_PROJECT_ID"]}/pipelines/#{ENV["CI_PIPELINE_ID"]}"))
+  @event = JSON.parse(File.read("https://www.gitlab.com/api/v4/projects/#{ENV["CI_PROJECT_ID"]}/pipelines/#{ENV["CI_PIPELINE_ID"]}"))
   if ENV["CI_MERGE_REQUEST_TARGET_BRANCH_NAME"] != ''
     @is_pull_request = ENV["CI_MERGE_REQUEST_TARGET_BRANCH_NAME"]
   end
