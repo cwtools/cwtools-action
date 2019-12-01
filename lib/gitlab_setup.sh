09dee54a1e260cb729fec09d961cd0b746d75de2
Thomas Boby <th.boby@gmail.com>
2019-12-01 18:50:35 +0000

    Update gitlab_setup.sh

--- lib/gitlab_setup.sh
+++ lib/gitlab_setup.sh
@@ -14,7 +14,7 @@ fi
 apt-get update && apt-get -y install ruby bash git wget p7zip
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/$INPUT_REVIEWDOGREF/install.sh| sh -s -- -b /usr/local/bin/
 
-git fetch origin $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
+#git fetch origin $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
 
 cd /
 git clone --depth=1  --single-branch --branch $INPUT_CWTOOLSACTIONREF https://github.com/cwtools/cwtools-action.git action
