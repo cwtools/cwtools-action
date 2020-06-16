dfc814e1f994e803598a8ac7318a720709391bd0
Antoni Baum <antoni.baum@protonmail.com>
2020-06-17 00:21:14 +0200

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -102,7 +102,7 @@ if [ $CW_CI_ENV = "gitlab" ]; then
   cd $CW_WORKSPACE
   if [ -f errors.txt ]; then
     echo "Running reviewdog on $PWD/errors.txt..."
-    if reviewdog -conf=/action/lib/.reviewdog.yml -name="$CW_CHECKNAME" -reporter=gitlab-mr-discussion | grep -m 1 -q ":E:"; then
+    if reviewdog -conf=/action/lib/.reviewdog.yml -name="$CW_CHECKNAME" -reporter=gitlab-mr-discussion | grep -m 1 -q "❌"; then
       echo "At least one error in annotated files! Exiting with a non-zero error code..."
       exit 1
     fi
